f589ab72fdc675d8a989ce021d8bef2a97529851
rsvincent <rvincent87@gmail.com>
2024-02-04 23:34:59 -0500

    updates + 404 wip

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const express = require("express");
 const app = express();
 const config = { port: process.env.PORT || 3000 };
 const shoppingList = require("./models/shoppingList");
-const mongoDB =
-  "mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD@cluster0.lohdtbg.mongodb.net/?retryWrites=true&w=majority";
-
+// const mongoDB = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lohdtbg.mongodb.net/?retryWrites=true&w=majority`;
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.lohdtbg.mongodb.net/?retryWrites=true&w=majority`
+);
 app.get("/shoppingList", (req, res) => {
   shoppingList.find().then((results) => res.status(200).json(results));
 });
--- models/shoppingList.js
+++ models/shoppingList.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 
 const listSchema = new mongoose.Schema({
-  listId: Number,
-  title: String,
-  updatedOn: Date,
-  items: [
-    {
-      itemId: Number,
-      itemName: String,
-    },
-  ],
+  listId: { type: Number },
+  title: { type: String, maxLength: 50 },
+  createdAt: { type: Date, default: Date.now, required: true },
+  updatedAt: { type: Date, default: Date.now },
+  //   items: [
+  //     {
+  //       itemId: Number,
+  //       itemName: String,
+  //     },
+  //   ],
 });
 
 module.exports = mongoose.model("shoppingList", listSchema);
